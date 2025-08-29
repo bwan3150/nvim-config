e707b0021ffb301ffcee184974e4d46215f8f3fc
EricWangKonec <eric.wang@konec.com.au>
2025-08-29 10:02:47 +1000

    Update: default enable relative row num

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,10 @@
 require "nvchad.options"
 
 -- add yours here!
+local o = vim.o
 
--- local o = vim.o
+-- 开启相对行号
+o.relativenumber = true
+o.number = true  -- 当前行显示绝对行号
+
 -- o.cursorlineopt ='both' -- to enable cursorline!
